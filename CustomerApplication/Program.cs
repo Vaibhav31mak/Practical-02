fbe73bb9edff2d63bb5c677cdc912527dfb31113
xyz <vaibhav.makwana@simformsolutions.com>
2026-03-26 10:00:36 +0530

    Used primary constructors and added commments for readibility

--- CustomerApplication/Program.cs
+++ CustomerApplication/Program.cs
@@ -1,6 +1,5 @@
 ﻿using CustomerApplication;
 
-Console.WriteLine("Summary:-\nMethod: PrintInfo()\nPrints customer account details including bank name, account number, and customer name.\nReturn Type: void");
 CustomerAccount.BankName = "State Bank Of India";
 CustomerAccount account = new CustomerAccount(123456789, "Vaibhav");
 
